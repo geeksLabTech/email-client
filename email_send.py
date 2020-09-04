5defaa4cc16a21c949857bfe39545afa0ca79f6c
Javier Oramas <javiale2000@gmail.com>
2020-09-04 18:52:35 -0400

    code comment on email send

--- email_send.py
+++ email_send.py
@@ -4,11 +4,11 @@ import smtplib
 
 def send_mail(sender, pwd, to, subject, text):
     
-    smtpserver = smtplib.SMTP_SSL(host='smtp.estudiantes.matcom.uh.cu', port=465)
-    smtpserver.ehlo()
-    smtpserver.login(sender,pwd)
-    msg = 'Subject:'+subject+'\n\n'+text
-    smtpserver.sendmail(sender,to,msg)
-    
-    smtpserver.close()
+    smtpserver = smtplib.SMTP_SSL(host='smtp.estudiantes.matcom.uh.cu', port=465)   #create connection with the smtp server
+    smtpserver.ehlo()                                                               #send enhaced HELO to the server to identify with the server
+    smtpserver.login(sender,pwd)                                                    #login in the server with the credentials given
+    msg = 'Subject:'+subject+'\n\n'+text                                            #create the email 
+    smtpserver.sendmail(sender,to,msg)                                              #send the email
+
+    smtpserver.close()                                                              #close connection
     
