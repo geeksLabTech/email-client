3fed930f97e7a8713ac837b969fddc7a9ed62459
Javier Oramas <javiale2000@gmail.com>
2020-09-04 11:46:54 -0400

    changed tls for ssl

--- email_send.py
+++ email_send.py
@@ -4,11 +4,11 @@ import smtplib
 
 def send_mail(sender, pwd, to):
     
-    smtpserver = smtplib.SMTP(host='smtp.estudiantes.matcom.uh.cu', port=965)
-    # print('here')
+    smtpserver = smtplib.SMTP_SSL(host='smtp.estudiantes.matcom.uh.cu', port=465)
+    print('here')
     smtpserver.ehlo()
-    smtpserver.starttls()
-    smtpserver.ehlo()
+    # smtpserver.startssl()
+    # smtpserver.ehlo()
     smtpserver.login(sender,pwd)
     msg = 'Subject:probando script\n esto esta mandado con el script'#+subject+'\n'+text
     smtpserver.sendmail(sender,to,msg)
