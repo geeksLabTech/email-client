6fa3fada1de2b475ffc035b3046766359a916d05
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2020-09-04 13:12:59 -0500

    Implemented_bot_basic_functionality

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = "here goes your access token from BotFather"
-bot_user_name = "the username you entered"
+bot_token = "1327859600:AAEcVOeg_0uWbR29lPoLyRNX0LcHgqSKIsI"
+bot_user_name = "experimental_email_bot"
 URL = "the heroku app link that we will create later"
