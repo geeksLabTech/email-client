bd9aa17fb3ebaefce6bec803a939a8bafdee9dd1
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2020-09-04 16:54:24 -0500

    Added_keroku_app_url

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,4 +1,4 @@
 
 bot_token = "1327859600:AAEcVOeg_0uWbR29lPoLyRNX0LcHgqSKIsI"
 bot_user_name = "experimental_email_bot"
-URL = "the heroku app link that we will create later"
+URL = "https://intense-shelf-64505.herokuapp.com/"
