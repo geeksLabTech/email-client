5c6d9503d668b707bf1aa301537f6466386862b1
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2020-09-06 15:15:52 -0500

    Try_something_new

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from email_client.email_send import send_mail
 from email_client.email_get import recieve_mail
 
 config_data = read_config('./config/config_bot.json')
-app = Client(config_data['bot_user_name'], config_data['api_id'], config_data['api_hash'])
-
+client = Client(config_data['bot_user_name'], config_data['api_id'], config_data['api_hash'])
+app = client.run
 
 # @app.on_message(filters.text)
 # def responder(client,message):
@@ -28,6 +28,6 @@ def recieve_emails(client, message):
     message.reply_text(emails[0])
 
 if __name__ == '__main__':
-    app.run()
+    app()
     # main()
 
