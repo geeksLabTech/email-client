534cfb87d67bce076dfd7b159fd72eb6e5bce717
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2020-09-06 02:35:13 -0500

    Alternative_bot_implementation

--- app.py
+++ app.py
@@ -11,58 +11,51 @@ from telegram.ext.callbackcontext import CallbackContext
 
 from telegram.update import Update
 from telegram.bot import Bot
+from telegram.botcommand import BotCommand
 
 from telebot.credentials import URL, bot_user_name, bot_token
-from fastapi import FastAPI, Form
-import uvicorn
-import requests
+#from fastapi import FastAPI, Form
+#import uvicorn
+#import requests
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
-TOKEN = bot_token
-#bot = telegram.Bot(token=TOKEN)
-app = FastAPI()
-updater = Updater(token=TOKEN, use_context=True)
-PORT = int(os.environ.get('PORT', '8443'))
-dispatcher = updater.dispatcher
 
+#app = FastAPI()
 
 def start(update: Update, context: CallbackContext):
     bot: Bot = context.bot
     bot.send_message(
         chat_id = update.effective_chat._id_attrs,
-        text='Hola, espero serte util y que no te pierdas un correo de la facultad 😁'
+        text='Espero funcionar ya!!'
     )
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+def help_command(update: Update, context: CallbackContext):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+def echo(update: Update, context: CallbackContext):
+    update.message.reply_text(update.message.text)
 
 
-@app.get('/')
-def index():
-   return 'Funciona!!'
+def main():
+    updater = Updater(token=bot_token, use_context=True)
+    PORT = int(os.environ.get('PORT', '8443'))
+    dispatcher = updater.dispatcher
 
-@app.get('/set_webhook')
-def set_webhook():
-    try:
-        updater.start_webhook(listen="0.0.0.0",
-                              port=PORT,
-                              url_path=TOKEN)
+    start_handler = CommandHandler('start', start)
+    help_handler = CommandHandler('help', help)
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(help_handler)
 
-        updater.bot.set_webhook(URL + TOKEN)
-        updater.idle()
-        
-        return "webhook setup ok"
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=bot_token)
 
-    except Exception:
-        return "webhook setup failed"
-
-
-@app.post("/login/")
-def login(username: str = Form(...), password: str = Form(...)):
-    return {"username": username}
+    updater.bot.set_webhook(URL + bot_token)
+    updater.idle()
 
 if __name__ == '__main__':
-    uvicorn.run(app)
+    main()
    
