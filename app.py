610897d49adf77eb3cb2c2b9bd2c9d0d52bc7766
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2020-09-05 22:57:24 -0500

    Initial_setup_for_fastapi

--- app.py
+++ app.py
@@ -1,61 +1,67 @@
 # from logging import exception
-from flask import Flask, request
-import telegram
-# import re
+
+#from flask import Flask, request
+import os
+
+#import telegram
+
+from telegram.ext import Updater, CommandHandler
+import logging
+from telegram.ext.callbackcontext import CallbackContext
+
+from telegram.update import Update
+from telegram.bot import Bot
+
 from telebot.credentials import URL, bot_user_name, bot_token
-global bot
-global TOKEN
+from fastapi import FastAPI, Form
+import uvicorn
+import requests
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
 
 TOKEN = bot_token
-bot = telegram.Bot(token=TOKEN)
-app = Flask(__name__)
-
-@app.route('/{}'.format(TOKEN), methods=['POST'])
-def respond():
-    # retrieve the message in JSON and then transform it to Telegram object
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
-
-    chat_id = update.message.chat.id
-    msg_id = update.message.message_id
-
-    # Telegram understands UTF-8, so encode text for unicode compatibility
-    text = update.message.text.encode('utf-8').decode()
-    # for debugging purposes only
-    print("got text message :", text)
-    # the first time you chat with the bot AKA the welcoming message
-    if text == "/start":
-        # print the welcoming message
-        bot_welcome = """
-        Hola, espero serte util y que no te pierdas un correo de la facultad 😁
-        """
-        # send the welcoming message
-        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
-
-    else:
-        try:
-            # TODO receive,send... emails
-            foo = ''
-        except Exception:
-            # if things went wrong
-            bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
-
-    return 'ok'
+#bot = telegram.Bot(token=TOKEN)
+app = FastAPI()
+updater = Updater(token=TOKEN, use_context=True)
+PORT = int(os.environ.get('PORT', '8443'))
+dispatcher = updater.dispatcher
 
 
-@app.route('/set_webhook', methods=['GET', 'POST'])
+def start(update: Update, context: CallbackContext):
+    bot: Bot = context.bot
+    bot.send_message(
+        chat_id = update.effective_chat._id_attrs,
+        text='Hola, espero serte util y que no te pierdas un correo de la facultad 😁'
+    )
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+
+@app.get('/')
+def index():
+   return 'Funciona!!'
+
+@app.get('/set_webhook')
 def set_webhook():
-    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
-    if s:
+    try:
+        updater.start_webhook(listen="0.0.0.0",
+                              port=PORT,
+                              url_path=TOKEN)
+
+        updater.bot.set_webhook(URL + TOKEN)
+        updater.idle()
         return "webhook setup ok"
-    else:
+
+    except Exception:
         return "webhook setup failed"
 
 
-@app.route('/')
-def index():
-   return '.'
-
+@app.post("/login/")
+def login(username: str = Form(...), password: str = Form(...)):
+    return {"username": username}
 
 if __name__ == '__main__':
-   app.run(threaded=True)
+    uvicorn.run(app)
    
